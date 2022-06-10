1db11eed56316aff4e3e883bd2fc7f64d40eddfa
STP-User-08 <renanvanni13@gmail.com>
2022-06-10 17:28:20 -0300

    Exercicio de fixação de exceção

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,12 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Account;
-import entities.BusinessAccount;
-import entities.Circle;
-import entities.ImportedProduct;
-import entities.Pessoa;
-import entities.PessoaFisica;
-import entities.PessoaJuridica;
-import entities.ProductExFix;
-import entities.Rectangle;
-import entities.SavingsAccount;
-import entities.Shape;
-import entities.UsedProduct;
-import entities.enums.Color;
-import model.entities.Reservation;
+import model.entities.Account;
 import model.exceptions.DomainException;
+
 
 public class Program {
 
@@ -647,41 +628,69 @@ public class Program {
 //		}
 		
 //		EXCEÇÕES 3
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//		
+//		try {
+//			System.out.print("Room number: ");
+//			int number = sc.nextInt();
+//			System.out.print("Check-in date (dd/MM/yyyy): ");
+//			Date checkIn = sdf.parse(sc.next());
+//			System.out.print("Check-out date (dd/MM/yyyy): ");
+//			Date checkOut = sdf.parse(sc.next());
+//			
+//			Reservation reservation = new Reservation(number, checkIn, checkOut);
+//			System.out.println("Reservation: " + reservation);
+//			System.out.println();
+//			System.out.println("Enter date to update the reservation:");
+//			System.out.print("Check-in date (dd/MM/yyyy): ");
+//			checkIn = sdf.parse(sc.next());
+//			System.out.print("Check-out date (dd/MM/yyyy): ");
+//			checkOut = sdf.parse(sc.next());
+//			
+//			reservation.updateDates(checkIn, checkOut);
+//			System.out.println("Reservation: " + reservation);								
+//		}
+//		catch (ParseException e) {
+//			System.out.println("Invalid date format");
+//		}
+//		catch (IllegalArgumentException e) {
+//			System.out.println("Error in reservation: " + e.getMessage());
+//		}
+//		catch (DomainException e) {
+//			System.out.println("Error in reservation: " + e.getMessage());			
+//		}
+//		catch (RuntimeException e) {
+//			System.out.println("Unexpected error");
+//		}
 		
+//		EXERCICIO 10
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double limit = sc.nextDouble();
+		Account acc = new Account(number, holder, balance, limit);
+
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double withdraw = sc.nextDouble();
+
 		try {
-			System.out.print("Room number: ");
-			int number = sc.nextInt();
-			System.out.print("Check-in date (dd/MM/yyyy): ");
-			Date checkIn = sdf.parse(sc.next());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			Date checkOut = sdf.parse(sc.next());
-			
-			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-			System.out.println();
-			System.out.println("Enter date to update the reservation:");
-			System.out.print("Check-in date (dd/MM/yyyy): ");
-			checkIn = sdf.parse(sc.next());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sc.next());
-			
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);								
-		}
-		catch (ParseException e) {
-			System.out.println("Invalid date format");
-		}
-		catch (IllegalArgumentException e) {
-			System.out.println("Error in reservation: " + e.getMessage());
+			acc.withdraw(withdraw);
+			System.out.println(acc);
 		}
 		catch (DomainException e) {
-			System.out.println("Error in reservation: " + e.getMessage());			
+			System.out.println(e.getMessage());
 		}
 		catch (RuntimeException e) {
 			System.out.println("Unexpected error");
 		}
-			
+		
 		sc.close();
 	}
 	
--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -1,5 +1,7 @@
 package model.exceptions;
 
+// Exception -> obriga a tratar ou propagar 
+// RuntimeException -> compilador não obriga
 public class DomainException extends Exception {
 	private static final long serialVersionUID = 1L;
 
